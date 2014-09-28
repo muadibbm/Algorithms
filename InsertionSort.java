7c4bce07f2996aa14ffc6421c0ea21733a8ad6a7
Mehrdad Dehdashti <muadibbm@yahoo.com>
2014-09-27 21:29:48 -0400

    Added Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+
+public class InsertionSort 
+{
+	static void INSERTIONSORT (int [] A) {
+		int value;
+		int j;
+		for(int i = 1; i < A.length; i++) {
+			value = A[i];
+			j = i - 1;
+			while(A[j] > value) {
+				A[j+1] = A[j];
+				j = j - 1;
+				if(j < 0)
+					break;
+			}
+			A[j+1] = value;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int [] A = {2,8,7,1,3,5,6,4};
+		System.out.print("Unsorted List: ");
+		for(int i=0; i < A.length; i++)
+			System.out.print(A[i]+" ");
+		INSERTIONSORT(A);
+		System.out.print("\nSorted List: ");
+		for(int i=0; i < A.length; i++)
+			System.out.print(A[i]+" ");
+	}
+}
